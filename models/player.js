7983f68f1cc898c5c0c7b661c8f2d7d4e35e87c9
Vincent Olivert Riera <vincent.olivert.riera@youview.com>
2021-03-20 12:39:33 +0100

    Add availability to the Player model

--- models/player.js
+++ models/player.js
@@ -4,6 +4,15 @@ const Schema = mongoose.Schema;
 const playerSchema = new Schema({
   name: String,
   level: Number,
+  availability: {
+    monday: [String],
+    tuesday: [String],
+    wednesday: [String],
+    thursday: [String],
+    friday: [String],
+    saturday: [String],
+    sunday: [String],
+  }
 });
 
 module.exports = mongoose.model("Player", playerSchema);
