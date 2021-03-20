c2d1683101a64c6347e4ccd2844155019abb33e0
Vincent Olivert Riera <vincent.olivert.riera@youview.com>
2021-03-20 18:32:32 +0100

    Enable CORS

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const schema = require("./schema/schema");
 const PORT = process.env.PORT || 5000;
 const mongoose = require("mongoose");
 const uri = process.env.MONGODB_URI;
+const cors = require("cors");
 
 mongoose.connect(uri);
 mongoose.connection.once("open", () => {
@@ -12,14 +13,41 @@ mongoose.connection.once("open", () => {
 
 const app = express();
 
+// Origin config object
+origin_conf = {
+  // Origin undefined handler
+  originUndefined: function (req, res, next) {
+    if (!req.headers.origin) {
+      res.status(403).render();
+    } else {
+      next();
+    }
+  },
+  // Cross Origin Resource Sharing Options
+  cors: {
+    // Origin handler
+    origin: function (origin, cb) {
+      // setup a white list
+      const wl = (process.env.ALLOWED_ORIGINS || "").split(",");
+
+      if (wl.includes(origin)) {
+        cb(null, true);
+      } else {
+        cb(new Error(`Invalid origin: ${origin}`), false);
+      }
+    },
+    optionsSuccessStatus: 200,
+  },
+};
+
+app.use(origin_conf.originUndefined, cors(origin_conf.cors));
+
 // This route will be used as an endpoint to interact with Graphql.
 // All queries will go through this route.
 app.use("/graphql", graphqlHTTP({
   // Directing express-graphql to use this schema to map out the graph 
   schema,
-  // Directing express-graphql to use graphiql when goto "/graphql" address in
-  // the browser, which provides an interface to make GraphQl queries.
-  graphiql: true
+  graphiql: false,
 }));
 
 app.listen(PORT, () => {
